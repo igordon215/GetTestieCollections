8fe3c3530bd7d702fbc8537b502a2334b484def3
igordon215 <igordon215@gmail.com>
2024-07-27 11:03:15 -0400

    arrayDeque

---
+++ gettestie/src/test/java/rocks/zipcode/ArrayDequeTest.java
@@ -0,0 +1,55 @@
+package rocks.zipcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeTest {
+
+    @Test
+    public void calculateSizeTest() {
+        ArrayDeque<String> cerealArr = new ArrayDeque<>();
+        cerealArr.add("FruitLoops");
+        cerealArr.add("CaptnCrunch");
+        cerealArr.add("FrostedFlakes");
+
+        Integer expected = 3;
+
+        Integer actual = cerealArr.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTest() {
+        ArrayDeque<String> cerealArr = new ArrayDeque<>();
+        cerealArr.add("FruitLoops");
+        cerealArr.remove("FruitLoops");
+
+        Assert.assertTrue(cerealArr.isEmpty());
+    }
+
+    @Test
+    public void containsTest() {
+        ArrayDeque<String> cerealArr = new ArrayDeque<>();
+        cerealArr.add("FruitLoops");
+        cerealArr.add("CaptnCrunch");
+        cerealArr.add("FrostedFlakes");
+
+        Assert.assertTrue(cerealArr.contains("FrostedFlakes"));
+    }
+
+    @Test
+    public void clearIsEmpty() {
+        ArrayDeque<String> cerealArr = new ArrayDeque<>();
+        cerealArr.add("FruitLoops");
+        cerealArr.add("CaptnCrunch");
+        cerealArr.add("FrostedFlakes");
+        cerealArr.clear();
+
+        Assert.assertTrue(true);
+    }
+
+
+}
