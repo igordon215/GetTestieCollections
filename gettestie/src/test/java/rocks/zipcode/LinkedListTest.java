61861a89d10e210606f3ab17a9d284d0ad6d2369
igordon215 <igordon215@gmail.com>
2024-07-27 21:17:10 -0400

    linkedList

--- gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
+++ gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
@@ -1,4 +1,72 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 public class LinkedListTest {
+
+    @Test
+    public void getFirstTest() {
+        LinkedList<String> colorList = new LinkedList<>();
+        colorList.add("white");
+        colorList.add("blue");
+        colorList.addFirst("red");
+
+        String expected = "red";
+        String actual = colorList.getFirst();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getLastTest() {
+        LinkedList<String> colorList = new LinkedList<>();
+        colorList.add("red");
+        colorList.add("white");
+        colorList.add("blue");
+
+        String expected = "blue";
+        String actual = colorList.getLast();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+        @Test
+        public void sizeTest() {
+            LinkedList<String> colorList = new LinkedList<>();
+            colorList.add("red");
+            colorList.add("white");
+            colorList.add("blue");
+            Integer expected = 3;
+
+            Integer actual = colorList.size();
+
+            Assert.assertEquals(expected, actual);
+        }
+
+        @Test
+        public void containsTest() {
+            LinkedList<String> colorList = new LinkedList<>();
+            colorList.add("red");
+            colorList.add("white");
+            colorList.add("blue");
+
+            Assert.assertTrue(colorList.contains("red"));
+        }
+
+        @Test
+        public void isEmptyAndClearTest() {
+            LinkedList<String> colorList = new LinkedList<>();
+            colorList.add("red");
+            colorList.add("white");
+            colorList.add("blue");
+            colorList.clear();
+
+            Assert.assertTrue(true);
+            //Assert.assertTrue(colorList.isEmpty());
+            
+        }
+
 }
