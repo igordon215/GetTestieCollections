015b1161e39f10d2056b0378a2403706aed66371
igordon215 <igordon215@gmail.com>
2024-07-27 22:20:53 -0400

    iterator

--- gettestie/src/test/java/rocks/zipcode/IteratorTest.java
+++ gettestie/src/test/java/rocks/zipcode/IteratorTest.java
@@ -1,4 +1,27 @@
 package rocks.zipcode;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class IteratorTest {
+
+    @Test
+    public void iteratorTest() {
+        ArrayList<String> starks = new ArrayList<>();
+        starks.add("Arya");
+        starks.add("Bran");
+        starks.add("Robb");
+        starks.add("Rickon");
+        starks.add("Sansa");
+
+        Iterator itr = starks.iterator();
+
+        while (itr.hasNext()) {
+            String stark = (String) itr.next();
+            System.out.println(stark);
+        }
+    }
+
 }
