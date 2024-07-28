8c78d1d3d6d55149efc6126cfbf045868fc0c1f0
igordon215 <igordon215@gmail.com>
2024-07-27 22:38:11 -0400

    lab done

--- gettestie/src/test/java/rocks/zipcode/ComparableTest.java
+++ gettestie/src/test/java/rocks/zipcode/ComparableTest.java
@@ -1,4 +1,16 @@
 package rocks.zipcode;
 
+import org.junit.Test;
+
 public class ComparableTest {
+
+    @Test
+    public void sortTest() {
+        Integer[] intArr = new Integer[4];
+        intArr[0] = 3;
+        intArr[1] = 6;
+        intArr[2] = 9;
+        intArr[3] = 12;
+    }
+
 }
